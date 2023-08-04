3a8b7e9b4e65a31b39ae205e55af6e24acce03fa
cschweda <cschweda@gmail.com>
2023-08-04 09:59:33 -0500

    fix: images for Hub in /public/images/

--- creators/createHubImages.mjs
+++ creators/createHubImages.mjs
@@ -3,6 +3,20 @@ const require = createRequire(import.meta.url);
 const jsonfile = require("jsonfile");
 const fs = require("fs");
 const axios = require("axios");
+const dirpath = "./public/images";
+
+if (!fs.existsSync(dirpath)) {
+  fs.mkdirSync(dirpath);
+} else {
+  fs.rmdir("public/images", { recursive: true }, (err) => {
+    if (err) {
+      throw err;
+    }
+    console.log(`./public/images is deleted!`);
+    fs.mkdirSync(dirpath);
+    console.log(`./public/images is created!`);
+  });
+}
 
 const query = `query {
   articles(limit: 200, sort: "date:desc", where: { tags_contains: "infonet" }) {
@@ -17,5 +31,21 @@ axios
   .post("/graphql", { query, validateStatus: (status) => status === 200 })
   .then((res) => {
     const articles = res.data.data.articles;
+    writeImages(res.data.data.articles, ["splash"]);
   })
   .catch((err) => console.error(err));
+
+const writeImages = (items, attrs) =>
+  items.forEach((item) => attrs.forEach((attr) => writeImage(item, attr)));
+
+const writeImage = (item, attr) => {
+  const base64 = item[attr];
+  const data = base64.split(";base64,").pop();
+  const ext = base64.split("data:image/")[1].split(";")[0];
+  const path = `${dirpath}/${item._id}-${attr}.${ext}`;
+
+  fs.writeFile(path, data, "base64", (err) => {
+    if (err) throw err;
+  });
+  console.log(path);
+};
