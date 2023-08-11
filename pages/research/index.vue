42fb06c42616020a25be6bcfaaf911eb19bf4e09
cschweda <cschweda@gmail.com>
2023-08-11 09:46:25 -0500

    feat: scaffold out research page with tag query

---
+++ pages/research/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <pre>{{ query }}</pre>
+  </div>
+</template>
+
+<script setup>
+const route = useRoute();
+const query = route.query.tag;
+//console.log("tag value: ", route.query.tag);
+</script>
+
+<style lang="scss" scoped></style>
