0d63a143d9da2e140d998b2d574a65443739d906
cschweda <cschweda@gmail.com>
2023-08-11 13:39:37 -0500

    fix: scaffold out hub card for research page

--- pages/research/index.vue
+++ pages/research/index.vue
@@ -22,10 +22,97 @@
 
         <v-container fluid
           ><v-row>
-            <v-col cols="12">
-              <!-- {{ selectedTag }}<br /> -->
-              <div v-for="article in filteredArticles" :key="article._id">
-                <div class="pl-5 mb-5">
+            <v-col
+              v-for="article in filteredArticles"
+              :key="article._id"
+              class="mb-8"
+              cols="12"
+              md="6"
+              style="display: flex; flex-direction: column"
+            >
+              <v-card
+                height="100%"
+                style="position: relative"
+                class="px-5 py-5"
+              >
+                <div>
+                  {{ formatDate(article.date) }}
+                </div>
+                <div class="my-6 hover" style="font-weight: 900">
+                  {{ article.title }}
+                </div>
+                <v-img
+                  :src="`/images/${article._id}-splash.jpeg`"
+                  cover
+                  height="200"
+                  class="mb-5"
+                  :ref="'img_' + article._id"
+                  style="border: 1px solid #fafafa"
+                  alt="InfoNet article splash image"
+                >
+                  <template #placeholder>
+                    <v-row
+                      class="fill-height ma-0"
+                      align="center"
+                      justify="center"
+                    >
+                      <v-progress-circular
+                        indeterminate
+                        color="blue darken-3"
+                        aria-label="progress"
+                      ></v-progress-circular>
+                    </v-row>
+                  </template>
+                  <template v-slot:error>
+                    <!-- TODO: Fix this hacky fallback to PNG if error on image -->
+                    <v-img
+                      :src="`/images/${article._id}-splash.png`"
+                      cover
+                      height="200"
+                      class="mb-5"
+                      :ref="'img_' + article._id"
+                      style="border: 1px solid #fafafa"
+                      alt="InfoNet article splash image"
+                    ></v-img>
+                  </template>
+                </v-img>
+
+                <div>
+                  {{ truncateString(article.abstract, 350) }}
+                </div>
+                <div class="mt-5">
+                  <!-- {{ getInfoNetSpecificTags(article.tags) }} -->
+                  <span
+                    v-for="tag in getInfoNetSpecificTags(article.tags)"
+                    :key="article._id"
+                  >
+                    <v-chip
+                      size="x-small"
+                      style="font-weight: 700"
+                      @click="setTagFilter(tag)"
+                      >&nbsp;{{ tag.toUpperCase() }}&nbsp;</v-chip
+                    >
+                    &nbsp;
+                  </span>
+                </div>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn
+                    size="small"
+                    class="mt-5"
+                    variant="text"
+                    style="font-weight: 700"
+                    @click="gotoArticle(article.slug)"
+                  >
+                    Read Article&nbsp;&raquo;
+                  </v-btn>
+                </v-card-actions>
+              </v-card></v-col
+            >
+            <!-- <v-col cols="12"> -->
+            <!-- {{ selectedTag }}<br /> -->
+            <!-- <div v-for="article in filteredArticles" :key="article._id"> -->
+            <!-- <div class="pl-5 mb-5">
                   {{ article.title }}<br />
                   <span
                     v-for="tag in getInfoNetSpecificTags(article.tags)"
@@ -40,27 +127,27 @@
                     >
                     &nbsp;
                   </span>
-                </div>
-              </div>
-            </v-col></v-row
-          ></v-container
+                </div> -->
+
+            <!-- </div> -->
+            <!-- <v-container fluid>
+                <v-row align="stretch"> -->
+
+            <!-- <v-col
+                    v-for="tag in getInfoNetSpecificTags(article.tags)"
+                    :key="article._id"
+                    class="mb-8"
+                    cols="12"
+                    md="6"
+                    style="display: flex; flex-direction: column"
+                  >
+                    card here</v-col
+                  > -->
+            <!-- </v-row></v-container
+              > -->
+            <!-- </v-col> -->
+          </v-row></v-container
         >
-
-        <!-- <div v-for="article in filteredArticles" :key="article._id">
-          <div class="pl-5 mb-5">
-            {{ article.title }}<br />
-
-            <span
-              v-for="tag in getInfoNetSpecificTags(article.tags)"
-              :key="article._id"
-            >
-              <v-chip size="x-small" style="font-weight: 700"
-                >&nbsp;{{ tag.toUpperCase() }}&nbsp;</v-chip
-              >
-              &nbsp;
-            </span>
-          </div>
-        </div> -->
       </v-col>
     </v-row>
   </v-container>
@@ -82,6 +169,27 @@ if (tagFilter && tagFilter.length > 0) {
   filteredArticles = articles;
 }
 
+/**
+ * Formats a given date string into a human-readable format.
+ *
+ * @param {string} dateString - The date string to be formatted.
+ * @return {string} The formatted date string.
+ */
+const formatDate = (dateString) => {
+  const options = { year: "numeric", month: "long", day: "numeric" };
+  return new Date(dateString).toLocaleDateString(undefined, options);
+};
+
+const truncateString = (str, num = 250) => {
+  // If the length of str is less than or equal to num
+  // just return str--don't truncate it.
+  if (str.length <= num) {
+    return str;
+  }
+  // Return str truncated with '...' concatenated to the end of str.
+  return str.slice(0, num) + "...";
+};
+
 let selectedTag = ref([]);
 
 onMounted(() => {
@@ -92,7 +200,7 @@ onMounted(() => {
 watch(selectedTag, (newSelectedTag) => {
   if (newSelectedTag) {
     console.log("new watch tag: ", convertIndexToTag(newSelectedTag));
-    if (convertIndexToTag(newSelectedTag) === "all articles") {
+    if (convertIndexToTag(newSelectedTag) === "all research") {
       filteredArticles = articles.value;
     } else {
       filteredArticles = articles.value.filter(
@@ -115,6 +223,7 @@ const convertIndexToTag = (index) => {
 const setTagFilter = (tag) => {
   console.log("tag: ", tag, " index: ", infonetTags.value.indexOf(tag));
   selectedTag.value = [infonetTags.value.indexOf(tag)];
+  window.scrollTo(0, 0);
 };
 
 const getInfoNetSpecificTags = (tagArr) => {
@@ -128,6 +237,11 @@ const getInfoNetSpecificTags = (tagArr) => {
     }
   }
   return tagArrFiltered;
+};
+
+const gotoArticle = (slug) => {
+  let hubArticle = `https://icjia.illinois.gov/researchhub/articles/${slug}`;
+  return window.open(hubArticle, "_blank");
 };
 </script>
 
