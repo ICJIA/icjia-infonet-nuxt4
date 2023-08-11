1935843b6444ab5cdf29d5c23ca9d3112425cc26
cschweda <cschweda@gmail.com>
2023-08-11 11:33:01 -0500

    fix: scaffold chip group filter / watch

--- pages/research/index.vue
+++ pages/research/index.vue
@@ -1,6 +1,19 @@
 <template>
   <div>
     <pre v-if="tagFilter && tagFilter.length > 0">Filter: {{ tagFilter }}</pre>
+    <h2 class="text-h6 mb-2">Filter InfoNet Research:</h2>
+    <div class="text-center">
+      <v-chip-group v-model="selectedTag" column mandatory>
+        <v-chip
+          filter
+          variant="outlined"
+          v-for="index in infonetTags.length"
+          :key="index"
+        >
+          {{ infonetTags[index - 1] }}
+        </v-chip>
+      </v-chip-group>
+    </div>
     <div v-if="articles">
       <div v-for="article in filteredArticles" :key="article._id">
         <div class="pl-5 mb-5">
@@ -23,7 +36,8 @@
 
 <script setup>
 import hubArticles from "~/assets/json/hub.json";
-const infonetTags = useState("tags");
+let infonetTags = useState("tags");
+
 const articles = ref(hubArticles);
 let filteredArticles = ref([]);
 const route = useRoute();
@@ -35,6 +49,29 @@ if (tagFilter && tagFilter.length > 0) {
 } else {
   filteredArticles = articles;
 }
+
+let selectedTag = ref([]);
+
+onMounted(() => {
+  selectedTag.value = [0];
+  filteredArticles = articles;
+});
+
+watch(selectedTag, (newSelectedTag) => {
+  if (newSelectedTag) {
+    console.log("new watch tag: ", convertIndexToTag(newSelectedTag));
+    // filteredArticles = articles.value.filter(
+    //   (article) => article.tags.includes(newSelectedTag.value) === true
+    // );
+  } else {
+    console.log("watch tag: ", convertIndexToTag(selectedTag.value));
+    filteredArticles = articles;
+  }
+});
+
+const convertIndexToTag = (index) => {
+  return infonetTags.value[index];
+};
 
 const getInfoNetSpecificTags = (tagArr) => {
   let tagArrFiltered = [];
