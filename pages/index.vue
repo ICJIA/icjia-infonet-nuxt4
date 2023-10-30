3987334f4700119a91e1e669f9e2ed5b243df787
cschweda <cschweda@gmail.com>
2023-10-30 08:44:43 -0500

    fix: typo on home

--- pages/index.vue
+++ pages/index.vue
@@ -210,7 +210,7 @@
                               margin-top: -35px;
                             "
                           >
-                            InfoNet Research
+                            InfoNet & Other Victim Services Research
                           </h2>
                         </v-col>
                         <v-col
@@ -374,7 +374,9 @@
                 </v-no-ssr>
               </div>
               <div v-if="isMobile && isMounted">
-                <h2 class="px-5 py-5 mb-10">InfoNet Research</h2>
+                <h2 class="px-5 py-5 mb-10">
+                  InfoNet & Other Victim Services Research
+                </h2>
 
                 <div v-for="article in articles" :key="article._id">
                   <v-card
